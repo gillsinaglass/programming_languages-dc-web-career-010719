40bd21beef68e450bca7a3dd501a184471799bad
Ryan Gill <rdgill95@gmail.com>
2019-01-07 00:53:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,12 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
-    language.each do |name, attributes|
+    language.each do |name, values|
       if new_hash.keys.include?(name)
         new_hash[name][:style] << style
       else
         new_hash[name] = {
-          :type => attributes[:type],
+          :type => values[:type],
           :style => [style]
         }
       end
